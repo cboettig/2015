6d2900e4d2266e3cad0531bd61583671f374b550
Carl Boettiger <cboettig@gmail.com>
2015-01-07 18:39:32 -0500

    still testing caching

--- _build.sh
+++ _build.sh
@@ -6,7 +6,10 @@ REPO=2015
 if [ "$DRONE_BRANCH" ]
   then 
     if [ "$DRONE_BRANCH" = "master" ]
-      then 
+      then
+        rsync -a --delete /var/cache/drone/src/github.com/cboettig/cache _cache/  
 	Rscript -e 'servr::jekyll(serve=FALSE, script="_build.R")'
+        rsync -a --delete _cache /var/cache/drone/src/github.com/cboettig/cache  
+
     fi
 fi
