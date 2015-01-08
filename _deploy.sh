3e407a5f426fb3c0063062ee76a4e720d8d13fab
nexus-9 <cboettig@gmail.com>
2015-01-07 22:04:54 -0800

    draft post on config

--- _deploy.sh
+++ _deploy.sh
@@ -9,7 +9,7 @@ if [ "$DRONE_BRANCH" ]
       then
 	## Deploy site: clone the gh-pages branch, rsync files, commit, and push
         cd .. && git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO deploy 
-        rsync -av $REPO/_site/ deploy/
+        rsync -a $REPO/_site/ deploy/
         cd deploy && git add -A . && git commit -m 'Site updated from drone' && git push
     fi
 fi
