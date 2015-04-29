bf62277e461d068e544ba56163d301f5edbb01ad
Carl Boettiger <cboettig@gmail.com>
2015-04-29 13:16:36 -0700

    manage paths more generically

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -2,10 +2,10 @@
 REPO=2015
 set -e
 
-cd .. 
-git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO deploy 
-rsync -a $REPO/_site/ deploy/
-cd deploy 
+git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
+rsync -a _site/ ../deploy/
+
+cd ../deploy 
 git add -A . 
 git commit -m 'Site updated from script' 
 git push
