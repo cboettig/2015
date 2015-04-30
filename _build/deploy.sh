12f0f56b51b6e89360a6acb347ab65d1a258669d
Carl Boettiger <cboettig@gmail.com>
2015-04-30 15:12:19 -0700

    try without linking volume

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -2,6 +2,8 @@
 REPO=2015
 set -e
 
+Rscript -e 'servr::jekyll(scripts = "_build/build.R", serve = FALSE)'
+
 git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
 rsync -a _site/ ../deploy/
 
