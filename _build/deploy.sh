506c0928c06fa53db8d8035f1c6fd0b904a1c5d2
Carl Boettiger <cboettig@gmail.com>
2015-04-30 17:34:16 -0700

    IT'S WORKING! now don't build twice

--- _build/deploy.sh
+++ _build/deploy.sh
@@ -2,8 +2,6 @@
 REPO=2015
 set -e
 
-Rscript -e 'servr::jekyll(scripts = "_build/build.R", serve = FALSE)'
-
 git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
 rsync -a _site/ ../deploy/
 
