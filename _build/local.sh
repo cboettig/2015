391df40ce201ee8ddcdda66ec3282db3a302c336
Carl Boettiger <cboettig@gmail.com>
2015-05-11 11:50:29 -0700

    pull latest images first

--- _build/local.sh
+++ _build/local.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-set -e
+YEAR=2015
 
+set -e
 source ~/.notebook-env.sh
 
+## always use latest images for these
+docker pull cboettig/2015-cache
+docker pull cboettig/2015
+
 ## Build using cached data
-docker create --name cache -v /data/_cache cboettig/2015-cache
-docker run --rm --volumes-from cache -v $(pwd):/data cboettig/2015 Rscript -e 'servr::jekyll(serve = FALSE, script = "_build/build.R")'
+docker create --name cache -v /data/_cache cboettig/${YEAR}-cache
+docker run --rm --volumes-from cache -v $(pwd):/data cboettig/${YEAR} Rscript -e 'servr::jekyll(serve = FALSE, script = "_build/build.R")'
 
 ## Deploy to GitHub
-docker run --rm -ti -v $(pwd):/data -e GH_TOKEN=$GH_TOKEN --entrypoint "/data/_build/deploy.sh" cboettig/2015
+docker run --rm -ti -v $(pwd):/data -e GH_TOKEN=$GH_TOKEN --entrypoint "/data/_build/deploy.sh" cboettig/${YEAR}
 
 ## Update cache to reflect build (local builds only)
 docker run --rm --volumes-from cache -v $(pwd):/backup busybox tar cvf /backup/backup.tar /data/_cache
 docker run --name cache2 -v $(pwd):/backup busybox tar -xf /backup/backup.tar
 
 ## commit and push updated cache container
-docker commit cache2 cboettig/2015-cache
-docker push cboettig/2015-cache
+docker commit cache2 cboettig/${YEAR}-cache
+docker push cboettig/${YEAR}-cache
 
 ## Clean up
 docker rm -v cache cache2
