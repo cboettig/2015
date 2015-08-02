226250983aa6367b76378a179dc1adc761b55e96
Carl Boettiger <cboettig@gmail.com>
2015-08-01 17:59:00 -0700

    don't display output from push

--- _build/build.sh
+++ _build/build.sh
@@ -8,10 +8,12 @@ cd / && tar -xf /root/cache.tar
 cd data && Rscript -e 'servr::jekyll(serve = FALSE, script = "_build/build.R")'
 
 ## Deploy to gh-pages
+{
 git clone -b gh-pages https://cboettig:${GH_TOKEN}@github.com/cboettig/$REPO ../deploy 
 rsync -a _site/ ../deploy/
 cd ../deploy 
 git add -A . 
 git commit -m 'Site updated from script' 
 git push
+} &> /dev/null
 
